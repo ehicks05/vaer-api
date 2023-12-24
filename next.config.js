2bf83de2c7734b226fe99d676531f6662bf5e887
Eric Hicks <ehicks05@yahoo.com>
2023-12-24 12:14:24 -0500

    proxy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 
 module.exports = () => {
-  const rewrites = () => {
-    return [
-      {
-        source: "/*",
-        destination: "https://api.openweathermap.org/*",
-      },
-    ];
-  };
   return {
-    rewrites,
+    
   };
 };
---
+++ pages/api/[...all].ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import httpProxy from "http-proxy";
+
+const API_PROXY_URL = "https://api.openweathermap.org";
+
+export const config = {
+  api: {
+    // Enable `externalResolver` option in Next.js
+    externalResolver: true,
+    bodyParser: false,
+  },
+};
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  new Promise((resolve, reject) => {
+    const proxy: httpProxy = httpProxy.createProxy({
+      target: API_PROXY_URL,
+      changeOrigin: true,
+    });
+
+    proxy.once("proxyReq", function (proxyReq, req, res, options) {
+      proxyReq.path = proxyReq.path.replace("/api", "");
+      proxyReq.path = proxyReq.path.replace(
+        "appid=",
+        `appid=${process.env.OWM_API_KEY}`
+      );
+      console.log(proxyReq.path);
+    });
+    proxy.once("proxyRes", resolve);
+    proxy.once("error", reject);
+    proxy.web(req, res);
+  });
+};
